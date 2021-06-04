1c5ef6458bab1b1caa4f665229d0d4562220c500
Jason L. Walker <jasonlanewalker@gmail.com>
2021-06-03 22:01:14 -0500

    added product list component

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'app/product.service';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[];
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this._productService.getProducts().subscribe(r => this.products = r);
+  }
+
+}
