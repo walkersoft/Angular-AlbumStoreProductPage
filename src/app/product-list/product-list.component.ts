4495f29c63d7d9ddfd15d6f3779147f3f7c8c38e
Jason L. Walker <jasonlanewalker@gmail.com>
2021-06-03 22:02:01 -0500

    tests are dumb!

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 import { Product } from '../product';
 
 @Component({
