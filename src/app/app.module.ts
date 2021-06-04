95dcf06b1af77e69b84da1eebad318fc08d31c01
Jason L. Walker <jasonlanewalker@gmail.com>
2021-06-03 22:25:40 -0500

    added in navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,13 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component'
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: "product/:id", component: ProductPageComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
@@ -23,6 +30,7 @@ import { ProductListComponent } from './product-list/product-list.component'
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     ProductService
